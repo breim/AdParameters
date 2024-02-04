4ae397a6a20e7a4241ae6c41f1605ed299f9e8ae
Henrique Breim <henrique@breim.com.br>
2024-02-04 19:59:06 -0300

    feat: create currency converter module

---
+++ services/currency_converter.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module CurrencyConverter
+  module_function
+
+  EXCHANGE_RATES = {
+    'EUR' => 1.0,
+    'USD' => 1.13,
+    'TYR' => 3.31,
+    'SEK' => 11.32
+  }.freeze
+
+  def convert_to_eur(amount, currency)
+    rate = EXCHANGE_RATES.fetch(currency, 1.0)
+    (amount.to_f / rate).round(4)
+  end
+end
