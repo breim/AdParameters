9fc4b48211ba1fad4e73cce858090bd70f427738
Henrique Breim <henrique@breim.com.br>
2024-02-04 11:28:43 -0300

    fix: rename file

---
+++ services/xml_reader.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'open-uri'
+
+class XMLReader
+  def initialize(xml_path)
+    @xml_path = xml_path
+  end
+
+  def read_xml
+    file = File.open(@xml_path)
+    @doc = Nokogiri::XML(file) { |config| config.strict.noblanks }
+  end
+
+  def extract_creatives
+    @doc.xpath('//Creative')
+  end
+
+  def extract_placements
+    @doc.xpath('//Placement')
+  end
+end
