cc1edb4eb8e27b2badaba05b3907892bf8c15afa
Henrique Breim <henrique@breim.com.br>
2024-02-04 20:06:37 -0300

    feat: setup app to run a webserver to placement search by minimum price

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
-require 'sinatra'
-require "sinatra/reloader" if development?
+# frozen_string_literal: true
 
-get '/' do
-  "Hello world!"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require_relative 'services/placement_search'
+
+class App < Sinatra::Base
+  get '/search' do
+    PlacementSearch.new(params).call
+  end
 end
